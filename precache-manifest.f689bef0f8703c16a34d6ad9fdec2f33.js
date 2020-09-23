b75ace3e939912d426078620745a253acd9df4cd
Colin Brayton <gringolalia@gmail.com>
2020-09-23 17:36:33 -0300

    deploy

---
+++ precache-manifest.f689bef0f8703c16a34d6ad9fdec2f33.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19421857e980d550acb1",
+    "url": "/css/about.62285886.css"
+  },
+  {
+    "revision": "f4f040496f7d91d9d2dd",
+    "url": "/css/app.11b83081.css"
+  },
+  {
+    "revision": "21605d7faa30a0fe4e13",
+    "url": "/css/chunk-vendors.a7a52080.css"
+  },
+  {
+    "revision": "20a42ef12aac41cac1c8969c5758948e",
+    "url": "/fonts/inkline.20a42ef1.woff"
+  },
+  {
+    "revision": "ebe25a09f8c6844b928dae1ffa65ac79",
+    "url": "/fonts/inkline.ebe25a09.ttf"
+  },
+  {
+    "revision": "ff99d3fb75eedf667974b02c42e391d2",
+    "url": "/fonts/inkline.ff99d3fb.eot"
+  },
+  {
+    "revision": "50b34689795acb1a653ab1825bf8690c",
+    "url": "/img/infernalmachine.50b34689.jpg"
+  },
+  {
+    "revision": "ce029b764e9f052e9a4a1c7fc28bbd2e",
+    "url": "/img/thewhofflag.ce029b76.jpg"
+  },
+  {
+    "revision": "78d7dde7175d1d5ef822bc1063fb0b3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19421857e980d550acb1",
+    "url": "/js/about.6b08d488.js"
+  },
+  {
+    "revision": "f4f040496f7d91d9d2dd",
+    "url": "/js/app.54c814d8.js"
+  },
+  {
+    "revision": "21605d7faa30a0fe4e13",
+    "url": "/js/chunk-vendors.eac8f3ce.js"
+  },
+  {
+    "revision": "46a28eb3d4ca68934ae4de68eb97c81b",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
